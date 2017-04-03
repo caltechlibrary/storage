1fd8fd2562743367b0a0d845d4ff415b45cd076b
R. S. Doiel <rsdoiel@gmail.com>
2017-04-03 11:25:45 -0700

    Initial Commit

---
+++ storage_test.go
@@ -0,0 +1,13 @@
+package storage
+
+import (
+	"testing"
+)
+
+func TestFileSystem(t *testing.T) {
+	t.Errorf("TestFileSystem() not implemented")
+}
+
+func TestS3(t *testing.T) {
+	t.Errorf("TestS3() not implemented")
+}
